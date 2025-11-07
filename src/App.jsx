3c77749f2e35059ab1b14c99e2bfb48af0aefa99
pramod <pramodmalviyalaptop@gmail.com>
2025-11-08 01:42:57 +0530

    fix about import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/navbar"
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
-import About from "./pages/About"
+import About from "./pages/about"
 import Contact from "./pages/Contact"
 import Cart from "./pages/Cart"
 import Orders from "./pages/Orders"
